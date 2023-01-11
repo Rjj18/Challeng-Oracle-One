ccc17bd597081d5afde13acffc6dc2460bd01686
Rjj18 <roger.psn12@gmail.com>
2023-01-11 18:20:32 -0300

    adicionada função copiar

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function criptografar () {
         .replace(/o/g, "ober")
         .replace(/u/g, "ufat");
 
-    console.log(resultadoCripto);
+    document.getElementById("saida").innerHTML = resultadoCripto;
 }
 
 function decifrar () {
@@ -18,11 +18,24 @@ function decifrar () {
         .replace(/ai/g, "a")
         .replace(/ober/g, "o")
         .replace(/ufat/g, "u");
-        
-        console.log(resultadoDecifra);
+    
+        document.getElementById("saida").innerHTML = resultadoDecifra;
 }
 
-
+function copiar() {
+    // Get the text field
+    var copyText = document.getElementById("myInput");
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+  
+    // Alert the copied text
+    alert("Copied the text: " + copyText.value);
+  }
 
 
  // Object.keys(encripta).forEach((key) => {
